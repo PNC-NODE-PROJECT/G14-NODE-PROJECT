9edd438c3570249c20022f32dc46e15190c2fa47
SREYMAO VORN <sreymao.vorn@pnc.lan>
2022-05-01 19:31:32 +0700

    edit the quiz front end

--- routers/questions_router.js
+++ routers/questions_router.js
@@ -36,7 +36,7 @@ router.post("/create", (req, res) => {
  * @param req: request id in the object
  * @param res: respone the message success or id not found
  */
-router.delete("/:id", (req, res) => {
+router.delete("/delete/:id", (req, res) => {
     let id = req.params.id
     let isDeleteQuestion = questionModel.removeQuestion(id)
 
